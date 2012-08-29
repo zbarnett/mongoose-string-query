8552e72250c4e32ad4a8872e6c8130c567e381bb
Jed Wood <jed@limechile.com>
2012-08-29 11:35:22 -0400

    refactored to be a Mongoose plugin. Still need to update tests

--- controller.js
+++ controller.js
@@ -1,21 +1,9 @@
 var Monster = require('./model');
-var mongooseApiQuery = require('./lib/mongoose-api-query');
 
 module.exports = function(req, res){
 
-  mongooseApiQuery(req.query, {
-    custom_params: function(key, val, searchParams) {
-      if (key === "zamboni") {
-        searchParams["monster_identification_no"] = 1;
-        return true;
-      }
-    },
-    model: Monster,
-    per_page: 100
-  }, function(query, attributes){
-    query.exec(function (err, results) {
-      res.send(results);
-    });
+  Monster.apiQuery(req.query).exec(function(err, monsters) {
+      res.send(monsters);
   });
 
 };
